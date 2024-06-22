d760f0e87333a76d1f5649d895f7490c414fbb52
Rohit Kotian <rohitkotian07@gmail.com>
2024-06-22 12:26:39 +0530

    additional commits

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,7 @@
 export default function ProductCard({data}) {
   
   return (
-    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow bg-gray-800 border-gray-700">
+    <div className="max-w-xs border rounded-lg shadow bg-gray-800 border-gray-700">
       <img class="w-full h-28 rounded" src={data.thumbnailUrl} alt="" />
       <div className="p-5">
         <h5 className="mb-2 text-xl font-bold tracking-tight text-white">
